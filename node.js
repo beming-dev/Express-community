c64f8f0940529c8d345bef876bfbf6847e8055ab
progmins <mingfordev@gmail.com>
2021-03-07 21:31:53 +0900

    Fix the way html is loaded to render

--- node.js
+++ node.js
@@ -6,11 +6,18 @@ const app = express();
 
 const template = require("./lib/template");
 
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  let html = template.HTML();
-  res.send(html);
+  //let html = template.HTML();
+  //res.send(html);
+  res.render("test.html");
+});
+
+app.get("/create", (req, res) => {
+  res.render("createTest.html");
 });
 
 app.listen(3001, () => console.log("Example"));
