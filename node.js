99519189bbe1332bdf7cfdbbc379cd8fd50395ee
beming-dev <mingfordev@gmail.com>
2021-03-09 17:56:54 +0900

    Feat create process

--- node.js
+++ node.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const bodyParser = require("body-parser");
 // const sanitizeHtml = require("sanitize-html");
 const express = require("express");
 const app = express();
@@ -18,6 +19,7 @@ const template = require("./lib/template");
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get("/", (req, res) => {
   //let html = template.HTML();
@@ -33,15 +35,33 @@ app.get("/", (req, res) => {
   // res.render("test.html");
 });
 
-app.get("/create", (req, res) => {
-  res.render("createTest.html");
+app.get("/create/:boardName", (req, res) => {
+  let css = "/css/createTest.css";
+  connection.query('SELECT * FROM board', function (error, results, fields) {
+    if (error) throw error;
+    let content = template.createContent(req.params.boardName);
+    let category = template.list(results);
+    let html = template.HTML(category, content, css);
+    console.log(html);
+    res.send(html);
+  });
+});
+
+//여기를 하세요
+app.post("/processCreate/:boardName", (req, res) =>{
+  let today = new Date(); 
+  let now = new Date().toISOString().slice(0, 19).replace('T', ' ');
+  connection.query(`INSERT INTO post (id, title, description, writer, date, board) VALUES (NULL, ${req.body.title}, ${req.body.description}, "익명",${now}, ${req.params.boardName})`,
+   function (error, results, fields) {
+
+   });
 });
 
 app.get("/board/:boardName", (req, res) =>{
   let css = "/css/post.css";
   connection.query('SELECT * FROM board', function (error, results, fields) {
     if (error) throw error;
-    let content = template.postContent();
+    let content = template.postContent(req.params.boardName);
     let category = template.list(results);
     let html = template.HTML(category, content, css);
     res.send(html);
