cd5b29370aef7336ff44d049bdd9aba81394e5e9
beming-dev <mingfordev@gmail.com>
2021-03-10 10:41:38 +0900

    Feat create process

--- node.js
+++ node.js
@@ -42,29 +42,31 @@ app.get("/create/:boardName", (req, res) => {
     let content = template.createContent(req.params.boardName);
     let category = template.list(results);
     let html = template.HTML(category, content, css);
-    console.log(html);
     res.send(html);
   });
 });
 
 //여기를 하세요
 app.post("/processCreate/:boardName", (req, res) =>{
-  let today = new Date(); 
-  let now = new Date().toISOString().slice(0, 19).replace('T', ' ');
-  connection.query(`INSERT INTO post (id, title, description, writer, date, board) VALUES (NULL, ${req.body.title}, ${req.body.description}, "익명",${now}, ${req.params.boardName})`,
+  let now = template.getDate(new Date());
+  connection.query(`INSERT INTO post VALUES (NULL, '${req.body.title}', '${req.body.description}', "익명",'${now}', '${req.params.boardName}')`,
    function (error, results, fields) {
-
+     if(error) throw error;
+     res.redirect(`/board/${req.params.boardName}`)
    });
 });
 
 app.get("/board/:boardName", (req, res) =>{
   let css = "/css/post.css";
-  connection.query('SELECT * FROM board', function (error, results, fields) {
+  connection.query('SELECT * FROM board', function (error, board, fields) {
     if (error) throw error;
-    let content = template.postContent(req.params.boardName);
-    let category = template.list(results);
-    let html = template.HTML(category, content, css);
-    res.send(html);
+    connection.query(`SELECT * FROM post WHERE board='${req.params.boardName}'`, function(error, postList, fields) {
+      if (error) throw error;
+      let content = template.postContent(req.params.boardName, postList);
+      let category = template.list(board);
+      let html = template.HTML(category, content, css);
+      res.send(html);
+    })
   });
 });
 
