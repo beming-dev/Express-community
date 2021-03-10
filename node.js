836a0099a7b73b8e72561c22cb1fffb8020d00e7
beming-dev <mingfordev@gmail.com>
2021-03-10 14:08:23 +0900

    Feat middleware to reduce code

--- node.js
+++ node.js
@@ -15,6 +15,7 @@ let connection = mysql.createConnection({
 connection.connect();
 
 const template = require("./lib/template");
+const { request } = require("http");
 
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
@@ -22,31 +23,35 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended:false}));
 
 
+//middleware
+app.get('*', (req, res, next) =>{
+  connection.query('SELECT * FROM board', function (error, results, fields) {
+    if(error) throw error;
+    req.boardList = results;
+    next();
+  });
+});
+
+
   //let html = template.HTML();
   //res.send(html);
 app.get("/", (req, res) => {
   let css = "/css/design.css";
-  connection.query('SELECT * FROM board', function (error, results, fields) {
-    if (error) throw error;
     connection.query(`SELECT * FROM post`, function(error, postList, fields) {
       if (error) throw error;
-      let content = template.mainContent(results, postList);
-      let category = template.list(results);
+      let content = template.mainContent(req.boardList, postList);
+      let category = template.list(req.boardList);
       let html = template.HTML(category, content, css);
       res.send(html);
-    });
   });
 });
 
 app.get("/create/:boardName", (req, res) => {
-  let css = "/css/createTest.css";
-  connection.query('SELECT * FROM board', function (error, results, fields) {
-    if (error) throw error;
+    let css = "/css/createTest.css";
     let content = template.createContent(req.params.boardName);
-    let category = template.list(results);
+    let category = template.list(req.boardList);
     let html = template.HTML(category, content, css);
     res.send(html);
-  });
 });
 
 //여기를 하세요
@@ -60,16 +65,13 @@ app.post("/processCreate/:boardName", (req, res) =>{
 });
 
 app.get("/board/:boardName", (req, res) =>{
-  let css = "/css/post.css";
-  connection.query('SELECT * FROM board', function (error, board, fields) {
-    if (error) throw error;
+    let css = "/css/post.css";
     connection.query(`SELECT * FROM post WHERE board='${req.params.boardName}'`, function(error, postList, fields) {
       if (error) throw error;
       let content = template.postContent(req.params.boardName, postList);
-      let category = template.list(board);
+      let category = template.list(req.boardList);
       let html = template.HTML(category, content, css);
       res.send(html);
-    })
   });
 });
 
