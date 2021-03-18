719f7d5e01a162a9673a640a0564c278c3ae50f0
beming-dev <mingfordev@gmail.com>
2021-03-18 17:36:49 +0900

    Add password check function

---
+++ public/js/register.js
@@ -0,0 +1,13 @@
+let password = document.getElementById("password");
+let passwordCheck = document.getElementById("passwordCheck");
+
+password.addEventListener('change', (e)=>{
+    if(password.innerText === passwordCheck.innerText){
+
+    }
+});
+passwordCheck.addEventListener('change', (e)=>{
+    if(password.innerText === passwordCheck.innerText){
+
+    }
+})
